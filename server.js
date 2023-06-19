2ba0cae485fb147b3afe97375a4a8824cbfb45f6
Forestsong22 <byu4n07@naver.com>
2023-06-19 14:51:31 +0900

    실시간 버스 조회 프로젝트 시작

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.static(__dirname + '/public'))
+
+
+
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log('서버 작동중 ');
+});
